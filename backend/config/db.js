ea0ca5197278bae7d91ae5479ae6a32e9d89c87f
rmkec111722203088 <sshr22084.it@rmkec.ac.in>
2026-03-26 12:18:06 +0530

    chatbot and bycrypt

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const db = mysql.createPool({
   host:     process.env.DB_HOST     || "localhost",
   user:     process.env.DB_USER     || "root",
-  password: process.env.DB_PASS     || "Pass",
-  database: process.env.DB_NAME     || "neuroassess",
+  password: process.env.DB_PASS     || "shreya",
+  database: process.env.DB_NAME     || "neuro",
   port:     process.env.DB_PORT     || 3306,
   waitForConnections: true,
   connectionLimit: 10,
